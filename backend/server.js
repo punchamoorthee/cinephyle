b86a87f60b61c2073aecaeaab5d834412faed4e7
nanup <nanup.personal+github@gmail.com>
2025-05-28 23:11:10 -0700

    feat(backend): integrate mongodb atlas connection using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
-require('dotenv').config(); // Load environment variables
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config(); // Load environment variables
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// Connect to database
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5001; // Use environment variable or default
@@ -8,14 +12,14 @@ const PORT = process.env.PORT || 5001; // Use environment variable or default
 // Middleware
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Patse JSON bodies
-app.use(express.urlencoded({extended: true})) // Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 // Basic routes for testing
-app.get('/', (req, res) => {
-    res.send('Cinpehyle backend is running!');
+app.get("/", (req, res) => {
+  res.send("Cinpehyle backend is running!");
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Backend server is running on http://localhost:${PORT}`);
+  console.log(`Backend server is running on http://localhost:${PORT}`);
 });
